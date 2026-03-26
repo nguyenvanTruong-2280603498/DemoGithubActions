2d038699e0fdabfb9c91ff60de79d5029e3cc634
nguyenvanTruong <nguyenvantruong@example.com>
2026-03-26 14:47:35 +0700

    Add GitHub Actions workflow for Docker build and Docker Hub push

---
+++ src/main/java/hutech/nvt/nguyenvantruong/NguyenvantruongApplication.java
@@ -0,0 +1,13 @@
+package hutech.nvt.nguyenvantruong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenvantruongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenvantruongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hutech/nvt/nguyenvantruong/NguyenvantruongApplicationTests.java
@@ -0,0 +1,13 @@
+package hutech.nvt.nguyenvantruong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenvantruongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
